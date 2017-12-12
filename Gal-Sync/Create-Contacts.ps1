226ac865f1b1a94b4d6aed008dbd8c4f5ada6543
Endre <endre.igesund@crayon.com>
2017-12-12 10:08:50 +0100

    save existing contacts in hashtable.

--- Gal-Sync/Create-Contacts.ps1
+++ Gal-Sync/Create-Contacts.ps1
@@ -8,9 +8,19 @@ Connect-EXOPSSession # use credentials from target tenant (Compac)
 $SourceUsers = Import-Csv -Path $csvfile -Delimiter ";" -Encoding Default | ?{$_.windowsemailaddress -like "*@tomra.com"}
 $createcount = 0
 
+$existingcontacts = Get-MailContact -ResultSize unlimited
+
+$ContactsTable =@{}
+$existingcontacts | %{
+    $ContactsTable.Add($_.windowsemailaddress,$_)
+
+}
+
 foreach($user in $SourceUsers){
-    if((Get-Recipient $($user.WindowsEmailAddress) -ErrorAction silentlycontinue) -eq $null){
+    Write-Host sjekker $($user.WindowsEmailAddress)
+    if(!$ContactsTable.Contains($($user.WindowsEmailAddress))){
         #doesn't exist - Create
+        Write-Host Oppretter $($user.WindowsEmailAddress)
         New-MailContact -Name $($user.DisplayName) -LastName $($user.LastName) -DisplayName $($user.DisplayName) -ExternalEmailAddress $($user.WindowsEmailAddress) -FirstName $($user.FirstName)
         $createcount += 1
     }
